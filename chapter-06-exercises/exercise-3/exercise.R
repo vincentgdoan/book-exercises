041b81e32e05375a88d748b2cbcb6ee2ce20b94e
Vincent Doan <vincentgdoan@gmail.com>
2020-10-11 19:50:34 -0700

    Finished Exercise 3 and 4 for Chapter 6

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,17 +2,28 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-
-
+add_three <- function(x) {
+  
+  post_add <- x + 3
+  post_add
+}
+add_three(1)
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
 
+ten <- add_three(7)
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
 
+imperial_to_metric <- function(feet, inches) {
+  meters <- feet * 0.3048 + inches * 0.0254
+  meters
+}
+imperial_to_metric(5, 11)
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
 
+height_in_meters <- imperial_to_metric(5,9)
--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -4,13 +4,17 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
-
+is_twice_as_long <- function(word1, word2) {
+  boolean <- nchar(word1) >= nchar(word2) * 2
+  boolean
+}
+is_twice_as_long("apple","it")
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
 
-
+is_twice_as_long("apple","five")
+is_twice_as_long("applee","six")
 
 # Define a function `describe_difference` that takes in two strings. The
 # function should return one of the following sentences as appropriate
@@ -18,7 +22,23 @@
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
 
+describe_difference <- function(word1, word2) {
+  if(nchar(word1) > nchar(word2)) {
+    n <- nchar(word1)-nchar(word2)
+    cat("Your first string is longer by",n,"characters")
+  }
+  else if(nchar(word1) < nchar(word2)) {
+    n <- nchar(word2)-nchar(word1)
+    cat("Your second string is longer by",n,"characters")
+  }
+  else {
+    print("Your strings are the same length!")
+  }
+}
 
+describe_difference("dog","cats")
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
-
+describe_difference("pumpkin", "organic")
+describe_difference("fall", "organic")
+describe_difference("autumn", "fall")

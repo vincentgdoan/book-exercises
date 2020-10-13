34c9d56dc3f04e15c5afc2279edab0f573bcaceb
Vincent Doan <vincentgdoan@gmail.com>
2020-10-13 12:23:10 -0700

    Completed Exercise 1 and 2 of Chapter 8

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,31 +1,33 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast <- c("bagel", "cream cheese", "coffee", "tea")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch <- c("fried rice", "water", "bell peppers")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals <- list(
+  breakfast = my_breakfast,
+  lunch = my_lunch
+)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-
+meals$dinner <- c("fried chicken", "steak", "lobster", "mashed potatoes")
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinner <- meals$dinner
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
+meals[[5]] <- meals$lunch
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
 
-
-
+early_meals <- meals[c("breakfast", "lunch")]
 ### Challenge ###
 
 # Create a list that has the number of items you ate for each meal
--- chapter-08-exercises/exercise-2/exercise.R
+++ chapter-08-exercises/exercise-2/exercise.R
@@ -2,41 +2,47 @@
 
 # Create a *list* of 10 random numbers. Use the `runif()` function to make a 
 # vector of random numbers, then use `as.list()` to convert that to a list
-
-
+number_vector <- c(runif(10))
+number_list <- as.list(number_vector)
 # Use `lapply()` to apply the `round()` function to each number, rounding it to 
 # the nearest 0.1 (one decimal place)
-
+number_list <- lapply(number_list, round, .1)
 
 # Create a variable 'sentence' that contains a sentence of text (something 
 # longish). Make the sentence lowercase; you can use a function to help.
-
+sentence <- "I want to go to sleep because I went to bed late."
 
 # Use the `strsplit()` function to split the sentence into a vector of letters.
 # Hint: split on `""` to split every character
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
-
+sentence_list <- strsplit(sentence, "")
 
 # Extract the vector of letters from the resulting list
-
+sentence_vector<-sentence_list[[1]]
 
 # Use the `unique()` function to get a vector of unique letters
-
-
+?unique()
+sentence_unique <- unique(sentence_vector)
 # Define a function `count_occurrences` that takes in two parameters: a letter 
 # and a vector of letters. The function should return how many times that letter
 # occurs in the provided vector.
 # Hint: use a filter operation!
+count_occurences <- function(letter_vector, letter) {
 
+  match <- letter == letter_vector
+  number_occurences <-sum(match)
+  number_occurences
+}
 
 # Call your `count_occurrences()` function to see how many times the letter 'e'
 # is in your sentence.
-
+count_occurences(sentence_vector, "s")
 
 # Use `sapply()` to apply your `count_occurrences()` function to each unique 
 # letter in the vector to determine their frequencies.
 # Convert the result into a list (using `as.list()`).
 
+list_occurence <- as.list(sapply(unique(sentence_vector), count_occurences, sentence_vector))
 
 # Print the resulting list of frequencies
-
+print(list_occurence)
